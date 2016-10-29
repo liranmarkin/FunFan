0ddc5859c24ec6fa6e1e5d2cc2bea8e2b31ee7fa
Liran Markin <liran.markin@gmail.com>
2016-10-29 18:06:02 +0300

    finished server.py

--- Server/server.py
+++ Server/server.py
@@ -6,13 +6,14 @@ import thread
 
 
 class Servo():
-    pin = 12
+    pin = None
     NEUTRAL = 7.5
     ZERO = 2.5
     FULL = 12.5
     servo = None
 
-    def __init__(self):
+    def __init__(self, pin):
+        self.pin = pin
         GPIO.setmode(GPIO.BOARD)
 
         GPIO.setup(self.pin, GPIO.OUT)
@@ -44,7 +45,8 @@ class Camera():
     def get_image(self):
         return cv2.imread(self.img_url, 0)
 
-    def __init__(self):
+    def __init__(self, camera_port):
+        self.camera_port = camera_port
         self.camera = cv2.VideoCapture(self.camera_port)
         self.pic_width = self.camera.get(3)
         self.pic_height = self.camera.get(4)
@@ -59,6 +61,8 @@ def shoot(camera):
         camera.take_picture()
 
 camera = None
+servo = None
+server = None
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
@@ -74,14 +78,22 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print "{} wrote:".format(self.client_address[0])
         print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        if self.data == "getImg":
+            self.request.sendall(camera.get_image())
+        elif self.data == "getParms":
+            self.request.sendall(camera.get_camera_params())
+        else:
+            try:
+                deg = float(self.data)
+                if deg < 0 or deg >= 180:
+                    self.request.sendall("Error")
+                    return
+                servo.turnTo(deg)
+                self.request.sendall("Success")
+            except:
+                self.request.sendall("Error")
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-
-    camera = Camera()
-    thread.start_new_thread(shoot, (camera))
+def start_server(HOST, PORT):
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
@@ -89,3 +101,16 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    #start camera on channel 1
+    camera = Camera(1)
+    thread.start_new_thread(shoot, (camera))
+
+    #start servo on pin 12
+    servo = Servo(12)
+
+
+    start_server(HOST, PORT)
