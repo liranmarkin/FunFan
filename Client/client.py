2f338fb7f009e80fa829edb9fb75e1d7c83bdd21
Liran Markin <liran.markin@gmail.com>
2016-10-30 21:37:32 +0200

    checking code

--- Client/client.py
+++ Client/client.py
@@ -85,6 +85,7 @@ if __name__ == '__main__':
 
     PD = PeopleDetector()
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.connect((HOST, PORT))
         print "connected2"
