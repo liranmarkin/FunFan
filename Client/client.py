d920d458db0aa75577ec891c6ad12adb89f0b4b7
Liran Markin <liran.markin@gmail.com>
2016-11-02 17:56:08 +0200

    going back to SocketServer

--- Client/client.py
+++ Client/client.py
@@ -35,9 +35,9 @@ def recvall(sock, count):
     while count > 0:
         print "getting newbuf: count = " + str(count)
         newbuf = sock.recv(count)
-        #print newbuf
-        #if not newbuf:
-        #    return None
+        print newbuf
+        if not newbuf or newbuf == '':
+            return None
         buf += newbuf
         count -= len(newbuf)
     print buf
